558cbf776ced453f99702bd7b08b01b5788f387c
yuting1214 <l501l501l@gmail.com>
2025-03-27 14:13:40 +0800

    [EHN] Add static deploy

--- flow_run.py
+++
@@ -1,9 +0,0 @@
-from demo_workflow import show_stars
-
-# Run the flow
-if __name__ == "__main__":
-    show_stars([
-        "PrefectHQ/prefect",
-        "pydantic/pydantic",
-        "huggingface/transformers"
-    ])
